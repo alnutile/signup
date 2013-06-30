abaaa58742f2cd78a141c509c34d82ac04966f3d
Alfred Nutile <alfrednutile@gmail.com>
2013-06-30 07:44:35 -0400

    Starting the foundations to the startup area

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Signup::Application.config.secret_token = 'bc431f3b1ac25e874e9240bd737a5eb01c923155283d9ba7969f0cbc72a29a694b094d7e65855a4c6b2d7c98e6bff9bb3177429cf5ecc750ba693e5210abf8d9'

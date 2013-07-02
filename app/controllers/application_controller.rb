388a5de22ec96e0247cd59eaa97700fbff0b6a38
Alfred Nutile <alfrednutile@gmail.com>
2013-07-01 20:47:15 -0400

    Show pages done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  def admin
-  	return true
-  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def product_sell_button(product)
+		if product.active == true
+		  return link_to "Subscribe", new_user_registration_path(:product => product.id), :class => 'btn btn-primary'
+		else
+		  return "Coming Soon..."
+		end
+  end
+
+  def admin 
+  	return true
+  end
 end
